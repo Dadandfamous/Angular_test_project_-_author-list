731fa4b989cd8fa5a45f79e6fc1be1361962c695
Martin <stuurmartinpost@gmail.com>
2019-06-30 17:07:11 +0200

    init master

---
+++ src/app/authors.component.ts
@@ -0,0 +1,28 @@
+
+
+import { AuthorsService } from './author/authors.service';
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'authors',
+    template: `
+              <h2>{{ title }}<h2>
+              <ul>
+                <li *ngFor="let author of authors">
+                    {{ author }}
+                </li>
+              </ul>
+              
+              ` 
+})
+
+
+export class AuthorsComponent {
+    title = "List of authors"
+    authors;
+
+    constructor(service: AuthorsService){
+        this.authors = service.getAuthors()
+    }
+ 
+} 
